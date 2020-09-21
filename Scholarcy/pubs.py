0bd1f7d6ec24659e1b406b65e2c0a127d8e6fd4b
dennis9707 <he0707@hotmail.com>
2020-09-21 20:39:09 +0800

    fix bug in register forms

--- Scholarcy/pubs.py
+++ Scholarcy/pubs.py
@@ -1,10 +1,12 @@
-import json
-
-from scholarly import scholarly
-
-query = scholarly.search_pubs("computer vision")
-pub = next(query)
-
-jsonStr = json.dumps(pub.bib)
-print(jsonStr)
-# print(type(pub.bibtex))
+# import json
+#
+# from scholarly import scholarly
+#
+# query = scholarly.search_pubs("computer vision")
+# pub = next(query)
+#
+# jsonStr = json.dumps(pub.bib)
+# print(jsonStr)
+# # print(type(pub.bibtex))
+import secrets
+print(secrets.token_hex(16))
--- config.py
+++ config.py
@@ -4,8 +4,7 @@ DEBUG = True
 # Define the application directory
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-
+SECRET_KEY = '3e152b6a62c84eba86fe3d73dea03cd4'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
 TEST_SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'test.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
